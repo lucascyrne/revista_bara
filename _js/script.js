0693d57b800e1bc92610c380454f545c4affdafa
Lucas Cyrne <cyrne18@gmail.com>
2018-06-25 21:13:50 -0300

    Finished home.html and added site 1st edition material. Much work to do yet.

---
+++ _js/script.js
@@ -0,0 +1,64 @@
+// NEWSLETTER START ------------------------------------------------------------
+
+;( function( $, window, document, undefined ) {
+    'use strict';
+
+    var form        = '.newsletter',
+        className   = 'newsletter-active',
+        email       = 'input[type="email"]';
+
+    $( form ).each( function()
+    {
+        var $form   = $( this ),
+            $email  = $form.find( email ),
+            val     = '';
+
+        $email.on( 'keyup.addClassWhenEmail', function()
+        {
+            val = $email.val();
+            $form.toggleClass( className, val != '' && /^([\w-\.]+@([\w-]+\.)+[\w-]{2,12})?$/.test( val ) );
+        });
+    });
+})( jQuery, window, document );
+
+;( function( window, document, undefined )
+{
+    'use strict';
+
+    var form        = '.newsletter',
+        className   = 'newsletter-active',
+        email       = 'input[type="email"]',
+
+        addEventListener = function( element, event, handler )
+        {
+            element.addEventListener ? element.addEventListener( event, handler ) : element.attachEvent( 'on' + event, function(){ handler.call( element ); });
+        },
+        forEach = function( elements, fn )
+        {
+            for( var i = 0; i < elements.length; i++ ) fn( elements[ i ], i );
+        },
+        addClass = function( element, className )
+        {
+            element.classList ? element.classList.add( className ) : element.className += ' ' + className;
+        },
+        removeClass = function( element, className )
+        {
+            element.classList ? element.classList.remove( className ) : element.className += element.className.replace( new RegExp( '(^|\\b)' + className.split( ' ' ).join( '|' ) + '(\\b|$)', 'gi' ), ' ' );
+        };
+
+    forEach( document.querySelectorAll( form ), function( $form )
+    {
+        var $email = $form.querySelectorAll( email );
+
+        if( $email.length )
+        {
+            $email = $email[ 0 ];
+            addEventListener( $email, 'keyup', function()
+            {
+                $email.value != '' && /^([\w-\.]+@([\w-]+\.)+[\w-]{2,12})?$/.test( $email.value ) ? addClass( $form, className ) : removeClass( $form, className );
+            });
+        }
+    });
+})( window, document );
+
+// NEWSLETTER END --------------------------------------------------------------
