ca9ec789ff4b8f9b382020762d6e6d5657ae63a3
Lucas Cyrne <cyrne18@gmail.com>
2018-07-04 21:09:54 -0300

    Made some strutural improvements. 90% to go.

--- _js/script.js
+++ _js/script.js
@@ -62,3 +62,18 @@
 })( window, document );
 
 // NEWSLETTER END --------------------------------------------------------------
+
+/* AUTO SLIDE */
+
+onload  = start;
+
+function start(){
+var i = 1;
+function Move(){
+	i = (i%4)+1; // 4 is the Number of image in slider
+	document.getElementById('i'+i).checked = true;
+}
+setInterval(Move,3000); //change img in 3 sec
+}
+
+/* AUTO SLIDE */
